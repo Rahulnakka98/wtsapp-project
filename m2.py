b68b7f39011328235d1b2857a0855c4d553bba9c
rahul <rahuln468@gmail.com>
2024-08-13 17:35:41 +0530

    y

---
+++ m2.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+
+num1 = 27
+num2 = 6
+
+# Add two numbers
+sub = num1 - num2
+
+# Display the sum
+print('The sub of {0} and {1} is {2}'.format(num1, num2, sub))
+
